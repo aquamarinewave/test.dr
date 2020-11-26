4dab9d229a5a1c290a77e71797c7c6e1b6c88ff9
aquamarinewave <50908486+aquamarinewave@users.noreply.github.com>
2020-11-26 23:00:31 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ </head>
+ <body> 
+    <header>
+        <div class="header"> 
+            <div class="logo-container">
+                <img class="logo" src="./image/logo.png" alt="TR-logo">
+            </div>
+
+            <div class="menu">
+                <a href="#header">Головна</a>
+                <a href="#info-work">Принципи роботи</a>
+                <a href="#test">Скринінг</a>
+                <a href="#consultations">Консультації</a>
+                <a href="#footer">Запис</a>
+            </div>
+
+        </div>
+    </header> 
